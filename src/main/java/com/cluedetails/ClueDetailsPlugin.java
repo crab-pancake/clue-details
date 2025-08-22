9afc318f866e49ad0a19a098d39464051e544277
mc <neggie333@gmail.com>
2025-08-22 21:18:53 +1000

    Reset logout timer on plugin shutdown

--- src/main/java/com/cluedetails/ClueDetailsPlugin.java
+++ src/main/java/com/cluedetails/ClueDetailsPlugin.java
@@ -251,6 +251,13 @@ public class ClueDetailsPlugin extends Plugin
 		{
 			infoBoxManager.removeInfoBox(timer);
 		}
+		String minutes_config = configManager.getConfiguration("logouttimer", "idleTimeout");
+		int minutes_parsed = 25;
+		if (minutes_config != null)
+		{
+			minutes_parsed = Integer.parseInt(minutes_config);
+		}
+		client.setIdleTimeout(50 * 60 * minutes_parsed);
 	}
 
 	private void startUpOverlays()
